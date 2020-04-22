d706ce5b043fef012ddadf9eb334f155b1a5a7d9
Zu Weihan <zuweihan1992@gmail.com>
2020-04-22 21:04:44 +0200

    updates

--- heuristic/__main__.py
+++ heuristic/__main__.py
@@ -10,7 +10,17 @@ def main():
     if len(sys.argv) < 2:
         raise ValueError(f"{sys.argv[0]}: expected file location.")
 
-    problem = Problem.from_file(sys.argv[1], delimiter=',')
+    problem = Problem.from_file(sys.argv[1])
+
+    alns = ALNS(default_rng(problem.instance))
+
+'''
+    for op in D_OPERATORS:
+        alns.add_destroy_operator(op)
+
+    for op in R_OPERATORS:
+        alns.add_repair_operator(op)
+'''
 
 
 if __name__ == "__main__":
--- heuristic/classes/Problem.py
+++ heuristic/classes/Problem.py
@@ -5,8 +5,8 @@ from itertools import product
 from typing import List
 
 import numpy as np
+import importlib.util
 
-from heuristic.constants import DEPOT
 from .Singleton import Singleton
 
 class Problem(metaclass=Singleton):
@@ -39,20 +39,72 @@ class Problem(metaclass=Singleton):
         return self._instance
 
     @property
-    def capacity(self) -> float:
-        return self._capacity
+    def veh_num(self) -> int:
+        return self._veh_num
 
     @property
-    def handling_cost(self) -> float:
-        return self._handling_cost
+    def veh_capacity(self) -> float:
+        return self._veh_capacity
 
     @property
-    def num_customers(self) -> int:
-        return self._num_customers
+    def veh_spd(self) -> np.ndarray:
+        return self._veh_spd
 
     @property
-    def num_stacks(self) -> int:
-        return self._num_stacks
+    def veh_km_cost(self) -> np.ndarray:
+        return self._veh_km_cost
+
+    @property
+    def veh_tour_num(self) -> int:
+        return self._veh_tour_num
+
+    @property
+    def veh_tour_cost(self) -> np.ndarray:
+        return self._veh_tour_cost
+
+    @property
+    def veh_shift_num(self) -> int:
+        return self._veh_shift_num
+
+    @property
+    def veh_shift_start(self) -> np.ndarray:
+        return self._veh_shift_start
+
+    @property
+    def veh_shift_end(self) -> np.ndarray:
+        return self._veh_shift_end
+
+    @property
+    def veh_shift_cost(self) -> np.ndarray:
+        return self._veh_shift_cost
+
+    @property
+    def cust_num(self) -> int:
+        return self._cust_num
+
+    @property
+    def cust_open_time(self) -> np.ndarray:
+        return self._cust_open_time
+
+    @property
+    def cust_close_time(self) -> np.ndarray:
+        return self._cust_close_time
+
+    @property
+    def cust_pickup_demand(self) -> np.ndarray:
+        return self._cust_pickup_demand
+
+    @property
+    def cust_deliver_demand(self) -> np.ndarray:
+        return self._cust_deliver_demand
+
+    @property
+    def cust_distances(self) -> np.ndarray:
+        return self._cust_distances
+
+    @property
+    def service_time(self) -> float:
+        return self._service_time
 
     @property
     def distances(self) -> np.ndarray:
@@ -62,10 +114,35 @@ class Problem(metaclass=Singleton):
     def from_file(cls, location: str) -> Problem:
         cls.clear()
 
-        data = np.genfromtxt(location)
+        spec = importlib.util.spec_from_file_location('data', location)
+        data = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(data)
 
         problem = cls()
 
-        problem._idx = int(data[0])
+        problem._instance = int(data.INSTANCE_INDEX)
 
+        problem._veh_num = int(data.VEH_NUM)
+        problem._veh_capacity = int(data.VEH_CAPACITY)
+        problem._veh_spd = data.VEH_SPEED
+        problem._veh_km_cost = data.VEH_KM_COST
 
+        problem._veh_tour_num = int(data.VEH_TOUR_NUM)
+        problem._veh_tour_cost = data.VEH_TOUR_COST
+
+        problem._veh_shift_num = int(data.VEH_SHIFT_NUM)
+        problem._veh_shift_start = data.VEH_SHIFT_START
+        problem._veh_shift_end = data.VEH_SHIFT_END
+        problem._veh_shift_cost = data.VEH_SHIFT_COST
+
+        problem._cust_num = int(data.CUST_NUM)
+
+        problem._cust_open_time = data.CUST_OPEN_TIME
+        problem._cust_close_time = data.CUST_CLOSE_TIME
+
+        problem._cust_pickup_demand = data.CUST_PICKUP_DEMAND
+        problem._cust_delivery_demand = data.CUST_DELIVERY_DEMAND
+
+        problem._cust_distances = data.CUST_DISTANCES
+
+        problem._service_time = float(data.SERVICE_TIME)
